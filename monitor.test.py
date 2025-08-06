8505af242fddf3e23cc1a829efd3cdf0f9c98679
SCKA-1482 <80355270+SCKA-1482@users.noreply.github.com>
2025-08-07 00:34:47 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -1,12 +1,19 @@
 import unittest
-from monitor import vitals_ok
+from monitor import vitals_ok, validate_vitals
 
+def no_alert(_):
+    pass  # do nothing, disables blinking during tests
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+    def test_vitals_ok_no_alert(self):
+        self.assertFalse(vitals_ok(94, 70, 98, alert_func=no_alert))
+        self.assertTrue(vitals_ok(98.6, 75, 98, alert_func=no_alert))
 
+    def test_validate_vitals(self):
+        self.assertEqual(validate_vitals(94, 70, 98), (False, 'Temperature critical!'))
+        self.assertEqual(validate_vitals(98.6, 75, 98), (True, 'All vitals normal.'))
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
+
+
