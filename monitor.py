29e66ac63ce21fe1640a1f76a93691353f10f2e7
SCKA-1482 <80355270+SCKA-1482@users.noreply.github.com>
2025-08-12 20:46:10 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,7 +1,7 @@
 import sys
 from time import sleep
 
-# --- I/O Handler (can be mocked for testing) ---
+
 def default_display_warning(message):
     print(message)
     for _ in range(6):
@@ -12,7 +12,7 @@ def default_display_warning(message):
         sys.stdout.flush()
         sleep(1)
 
-# --- Core logic ---
+
 
 def is_out_of_range(value, min_val, max_val):
     return value < min_val or value > max_val
@@ -31,7 +31,7 @@ def alert_abnormal_vitals(vitals, alert_handler=default_display_warning):
         alert_handler(f"{vital['name']} out of range! Value: {vital['value']} (Expected: {vital['min']} - {vital['max']})")
     return len(abnormal_vitals) == 0
 
-# --- Interface Function ---
+
 def vitals_ok(temperature, pulseRate, spo2, alert_handler=default_display_warning):
     vitals = [
         {"name": "temperature", "value": temperature, "min": 95, "max": 102},
