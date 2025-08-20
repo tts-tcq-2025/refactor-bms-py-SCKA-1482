19da56813153fb9651219ffd1eda48994a80b0f8
SCKA-1482 <80355270+SCKA-1482@users.noreply.github.com>
2025-08-20 23:02:30 +0530

    Update monitor_earlywarning.py

--- monitor_earlywarning.py
+++ monitor_earlywarning.py
@@ -1,59 +1,65 @@
-def default_display_warning(msg):
-    print(f"WARNING: {msg}")
+def default_display_warning(message):
+    print(f"WARNING: {message}")
 
-def get_warning_ranges(min_val, max_val, tolerance_percent=1.5):
-    tolerance = (max_val - min_val) * tolerance_percent / 100
-    return (min_val, min_val + tolerance), (max_val - tolerance, max_val)
+def get_warning_ranges():
+    return {
+        'temperature': (36.1, 37.2),
+        'pulseRate': (60, 100),
+        'spo2': (95, 100)
+    }
 
-def check_vitals(vitals, status_func, alert_handler):
-    """
-    vitals: list of dicts with keys 'name', 'value', 'min', 'max'
-    status_func: function that returns 'NORMAL', 'WARNING_LOW', 'WARNING_HIGH', 'ALERT_LOW', 'ALERT_HIGH'
-    alert_handler: function to handle alert or warning messages
-    Returns True if all vitals are normal, False otherwise.
-    """
-    all_ok = True
-    for vital in vitals:
-        status = status_func(vital)
-        if status == 'NORMAL':
-            continue
-        all_ok = False
-        message = f"{vital['name']} {status} Value: {vital['value']}"
-        alert_handler(message)
-    return all_ok
+def check_vitals(temperature, pulseRate, spo2):
+    warning_ranges = get_warning_ranges()
+    vitals = {
+        'temperature': 'normal',
+        'pulseRate': 'normal',
+        'spo2': 'normal'
+    }
 
-def simple_status(vital):
-    if vital['value'] < vital['min']:
-        return 'ALERT_LOW'
-    elif vital['value'] > vital['max']:
-        return 'ALERT_HIGH'
-    return 'NORMAL'
+    # Temperature
+    low, high = warning_ranges['temperature']
+    if temperature < low - 1.0:
+        vitals['temperature'] = 'alert'
+    elif temperature < low:
+        vitals['temperature'] = 'warning'
+    elif temperature > high + 1.0:
+        vitals['temperature'] = 'alert'
+    elif temperature > high:
+        vitals['temperature'] = 'warning'
 
-def warning_status(vital):
-    low_warn_range, high_warn_range = get_warning_ranges(vital['min'], vital['max'])
-    val = vital['value']
-    if val < vital['min']:
-        return 'ALERT_LOW'
-    elif val > vital['max']:
-        return 'ALERT_HIGH'
-    elif low_warn_range[0] <= val <= low_warn_range[1]:
-        return 'WARNING_LOW'
-    elif high_warn_range[0] <= val <= high_warn_range[1]:
-        return 'WARNING_HIGH'
-    return 'NORMAL'
+    # Pulse Rate
+    low, high = warning_ranges['pulseRate']
+    if pulseRate < low - 10:
+        vitals['pulseRate'] = 'alert'
+    elif pulseRate < low:
+        vitals['pulseRate'] = 'warning'
+    elif pulseRate > high + 10:
+        vitals['pulseRate'] = 'alert'
+    elif pulseRate > high:
+        vitals['pulseRate'] = 'warning'
 
-def vitals_ok(temperature, pulseRate, spo2, alert_handler=default_display_warning):
-    vitals = [
-        {'name': 'Temperature', 'value': temperature, 'min': 95, 'max': 102},
-        {'name': 'Pulse Rate', 'value': pulseRate, 'min': 60, 'max': 100},
-        {'name': 'SpO2', 'value': spo2, 'min': 90, 'max': 100},
-    ]
-    return check_vitals(vitals, simple_status, alert_handler)
+    # SPO2
+    low, high = warning_ranges['spo2']
+    if spo2 < low - 5:
+        vitals['spo2'] = 'alert'
+    elif spo2 < low:
+        vitals['spo2'] = 'warning'
 
-def vitals_ok_with_warning(temperature, pulseRate, spo2, alert_handler=default_display_warning):
-    vitals = [
-        {'name': 'Temperature', 'value': temperature, 'min': 95, 'max': 102},
-        {'name': 'Pulse Rate', 'value': pulseRate, 'min': 60, 'max': 100},
-        {'name': 'SpO2', 'value': spo2, 'min': 90, 'max': 100},
-    ]
-    return check_vitals(vitals, warning_status, alert_handler)
+    return vitals
+
+def alert_vitals_with_warning(vitals, alert_handler=default_display_warning):
+    for vital, status in vitals.items():
+        if status == 'alert':
+            alert_handler(f"{vital} is at alert level!")
+        elif status == 'warning':
+            alert_handler(f"{vital} is at warning level.")
+
+def vitals_ok(temperature, pulseRate, spo2, alert_handler=None, consider_warnings_as_abnormal=False):
+    vitals = check_vitals(temperature, pulseRate, spo2)
+    if alert_handler:
+        alert_vitals_with_warning(vitals, alert_handler)
+    if consider_warnings_as_abnormal:
+        return all(status == 'normal' for status in vitals.values())
+    else:
+        # warnings treated as ok
+        return all(status in ('normal', 'warning') for status in vitals.values())
